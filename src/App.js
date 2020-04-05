71379e35e6726488753f674cdfecbc6fe9f66bd5
Sabelo <sabelomtw@gmail.com>
2020-04-05 22:37:48 +0200

    Almost/Completely finished Authentication

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Signup from './Authentication/Signup';
-import Login from './Authentication/Login';
-import Navbar from './Navigation/Navbar';
-import Dashboard from './Dashboard/Dashboard';
-import Footer from './Footer/Footer';
+import Signup from './components/Authentication/Signup';
+import Login from './components/Authentication/Login';
+import Navbar from './components/Navigation/Navbar';
+import Home from './components/Home/Home';
+import Dashboard from './components/Dashboard/Dashboard';
+import Footer from './components/Footer/Footer';
+import { connect } from 'react-redux';
+import ProtectedRoute from './components/ProtectedRoute';
 
-const App = () =>{
+const App = (props) =>{
+  const { isAuthenticated, isVerifying } = props;
+
   return (
     <Router>
       <div className="App">
         <Navbar id='navbar'/>
         <Switch id='routes'>
-          <Route exact path='/' component={Dashboard}/>
+          <Route exact path='/home' component={Home}/>
           <Route path='/signup' component={Signup}/>
           <Route path='/login' component={Login}/>
+          <ProtectedRoute
+            path='/'
+            component={Dashboard}
+            isAuthenticated={isAuthenticated}
+            isVerifying={isVerifying}
+          />
         </Switch>
         <Footer id='footer'/>
       </div>
@@ -24,4 +34,11 @@ const App = () =>{
   );
 }
 
-export default App;
+const mapStateToProps = (state) =>{
+  return{
+    isAuthenticated: state.auth.isAuthenticated,
+    isVerifying: state.auth.isVerifying
+  }
+}
+
+export default connect(mapStateToProps)(App);
